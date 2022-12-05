ad2ac03e25029c4ff04877afeebd93a8f91a799f
Chloe <b.bernard@lyon.ort.asso.fr>
2022-12-05 13:17:36 +0100

    add pages

---
+++ public/pages/Contacts.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EQUIPES</title>
+</head>
+<body>
+<!-- sous menus equipes : u9 - u11 -u13 - u15 -u17 -seniors2 -seniors1 -->
+<!--1 page par equipe-->
+</body>
+</html>
